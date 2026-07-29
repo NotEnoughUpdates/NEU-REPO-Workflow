eeef281a07ebe6a118a8177f55cca247a6f9f06f
alex <8379108+Alex33856@users.noreply.github.com>
2026-07-29 13:03:58 -0400

    feat: also check for existence of items in parents

--- src/main/java/me/alex/workflow/checks/constants/CheckParents.java
+++ src/main/java/me/alex/workflow/checks/constants/CheckParents.java
@@ -6,6 +6,7 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import me.alex.workflow.checks.AbstractCheck;
 import me.alex.workflow.utils.FileUtils;
+import me.alex.workflow.utils.Items;
 
 import java.io.File;
 import java.util.List;
@@ -35,6 +36,7 @@ public class CheckParents implements AbstractCheck {
 			return false;
 		}
 
+		boolean isValid = true;
 		Set<String> seenItems = new ObjectOpenHashSet<>();
 		for (var entry : parents.entrySet()) {
 			String key = entry.getKey();
@@ -43,20 +45,30 @@ public class CheckParents implements AbstractCheck {
 			if (seenItems.contains(key)) {
 				logFileIssue(file, "Duplicate key: " + key);
 			} else seenItems.add(key);
+			if (!Items.ITEMS.contains(key)) {
+				logFileIssue(file, "Unknown parent item: " + key);
+				isValid = false;
+			}
 
 			Set<String> groupItems = new ObjectOpenHashSet<>();
 			for (String item : values) {
 				if (seenItems.contains(item)) {
 					logFileIssue(file, "Duplicate item: " + item);
+					isValid = false;
 				} else seenItems.add(item);
 				if (groupItems.contains(item)) {
 					logFileIssue(file, "Duplicate item in group: " + item);
+					isValid = false;
 				} else groupItems.add(item);
+				if (!Items.ITEMS.contains(item)) {
+					logFileIssue(file, "Unknown item: " + item);
+					isValid = false;
+				}
 			}
 			seenItems.addAll(groupItems);
 		}
 
-		return true;
+		return isValid;
 	}
 
 	public static final Codec<Object2ObjectMap<String, List<String>>> CODEC = CodecUtils.object2ObjectMapCodec(Codec.STRING, Codec.STRING.listOf());
